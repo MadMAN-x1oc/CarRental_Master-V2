0446cd3f00024f7dc94d9197ccc671b0f8650fd0
shariandabre <dabresharian@gmail.com>
2023-08-17 20:50:24 +0530

    ok

--- components/RideSelector.js
+++ components/RideSelector.js
@@ -1,7 +1,8 @@
-import {useEffect,useState} from 'react'
-import tw from 'tailwind-styled-components'
-import { carList } from '../data/carList'
-const RideSelector = ({  pickupCoordinates,
+import { useEffect, useState } from "react";
+import tw from "tailwind-styled-components";
+import { carList } from "../data/carList";
+const RideSelector = ({
+  pickupCoordinates,
   dropoffCoordinates,
   tripType,
   handlebooking,
@@ -13,51 +14,49 @@ const RideSelector = ({  pickupCoordinates,
   price,
   days,
   rideDistance,
-  setDistance,}) => {
-    const [showPopup, setShowPopup] = useState(false);
+  setDistance,
+}) => {
+  const [showPopup, setShowPopup] = useState(false);
 
-    useEffect(() => {
-      if (
-        pickupCoordinates[0] !== 0 &&
-        pickupCoordinates[1] !== 0 &&
-        dropoffCoordinates[0] !== 0 &&
-        dropoffCoordinates[1] !== 0
-      ) {
-        const calculateDistance = async () => {
-          const response = await fetch(
-            `https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoordinates[0]},${pickupCoordinates[1]};${dropoffCoordinates[0]},${dropoffCoordinates[1]}?access_token=pk.eyJ1IjoiZ29kaGEiLCJhIjoiY2xpZTh3ZWdoMDhkajNlcGRpZ3l0dTllOSJ9.uaD7_LAOgpJTzlhlRf9u9g`
-          );
-          const data = await response.json();
-          const distance = data.routes[0].distance / 1000;
-          if (tripType == "Round Trip") {
-            setDistance(distance.toFixed(0) * 2);
-          } else{
-            setDistance(distance.toFixed(0) );
-          }
-        };
-  
-        calculateDistance();
-      }
-    }, [pickupCoordinates, dropoffCoordinates,rideDistance, tripType]);
-  
-    const openPopup = (car) => {
-      setSelectedCar(car);
-      setShowPopup(true);
-    };
-  
-    const closePopup = () => {
-      setShowPopup(false);
-    };
-  
+  useEffect(() => {
+    if (
+      pickupCoordinates[0] !== 0 &&
+      pickupCoordinates[1] !== 0 &&
+      dropoffCoordinates[0] !== 0 &&
+      dropoffCoordinates[1] !== 0
+    ) {
+      const calculateDistance = async () => {
+        const response = await fetch(
+          `https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoordinates[0]},${pickupCoordinates[1]};${dropoffCoordinates[0]},${dropoffCoordinates[1]}?access_token=pk.eyJ1IjoiZ29kaGEiLCJhIjoiY2xpZTh3ZWdoMDhkajNlcGRpZ3l0dTllOSJ9.uaD7_LAOgpJTzlhlRf9u9g`
+        );
+        const data = await response.json();
+        const distance = data.routes[0].distance / 1000;
+        if (tripType == "Round Trip") {
+          setDistance(distance.toFixed(0) * 2);
+        } else {
+          setDistance(distance.toFixed(0));
+        }
+      };
+
+      calculateDistance();
+    }
+  }, [pickupCoordinates, dropoffCoordinates, rideDistance, tripType]);
+
+  const openPopup = (car) => {
+    setSelectedCar(car);
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+
   return (
     <Wrapper>
-      <Title>
-        Approximate distance: {rideDistance === 0 ? Package : rideDistance} km
-      </Title>
       <Carlist>
         {carList.map((car, index) => (
           <Car key={index}>
-            <Carimg className={`${car.imgUrl}`} ></Carimg>
+            <Carimg className={`${car.imgUrl}`}></Carimg>
             <div className="flex-1 gap-4 flex flex-col justify-evenly items-center md:flex-row">
               <CarDetails className="flex-1">
                 <Service>{car.service}</Service>
@@ -79,11 +78,10 @@ const RideSelector = ({  pickupCoordinates,
                       ? car[Package]
                       : formType === "OutStation"
                       ? tripType === "Round Trip"
-                        ? car.extraKm *
-                          (rideDistance > 300 ? rideDistance : 300) *
-                          days
-                        : car.extraKm *
-                          (rideDistance > 300 ? rideDistance : 300)
+                        ? (car.extraKm * 300 + 400) * days
+                        : rideDistance >= 500
+                        ? car.extraKm * 300 + 800
+                        : car.extraKm * 300 + 400
                       : ""
                   ).toLocaleString("en-IN", {
                     style: "currency",
@@ -100,11 +98,10 @@ const RideSelector = ({  pickupCoordinates,
                           ? car[Package]
                           : formType === "OutStation"
                           ? tripType == "Round Trip"
-                            ? car.extraKm *
-                              (rideDistance > 300 ? rideDistance : 300) *
-                              days
-                            : car.extraKm *
-                              (rideDistance > 300 ? rideDistance : 300)
+                            ? (car.extraKm * 300 + 400) * days
+                            : rideDistance >= 500
+                            ? car.extraKm * 300 + 800
+                            : car.extraKm * 300 + 400
                           : ""
                       ).toLocaleString("en-IN", {
                         style: "currency",
@@ -129,11 +126,10 @@ const RideSelector = ({  pickupCoordinates,
                       ? car[Package]
                       : formType === "OutStation"
                       ? tripType == "Round Trip"
-                        ? car.extraKm *
-                          (rideDistance > 300 ? rideDistance : 300) *
-                          days
-                        : car.extraKm *
-                          (rideDistance > 300 ? rideDistance : 300)
+                        ? (car.extraKm * 300 + 400) * days
+                        : rideDistance >= 500
+                        ? car.extraKm * 300 + 800
+                        : car.extraKm * 300 + 400
                       : ""
                   );
               }}
@@ -147,43 +143,42 @@ const RideSelector = ({  pickupCoordinates,
 
       {showPopup && selectedCar && (
         <Popup>
-          <div className="h-14 text-lg rounded-t-lg flex font-medium justify-center items-center text-white max-w-xl  bg-[#0080ff]  w-full md:w-5/6">
+          <div className="h-14 text-lg rounded-t-lg flex font-medium justify-center items-center text-white max-w-xl  bg-[#AF302F]  w-full md:w-5/6">
             <h2>Fare Breakup</h2>
           </div>
           <PopupContent>
-          <CloseButton onClick={closePopup}><img className="h-4" src="https://img.icons8.com/?size=512&id=46&format=png"/></CloseButton>
+            <CloseButton onClick={closePopup}>
+              <img
+                className="h-4"
+                src="https://img.icons8.com/?size=512&id=46&format=png"
+              />
+            </CloseButton>
             {formType === "OutStation" && tripType === "Round Trip" && (
               <ul className="flex flex-col gap-2">
                 <li>
                   &#x2022;{" "}
                   {"Outstation (Roundtrip " +
                     days +
-                    " day/s) "+
+                    " day/s) " +
                     price +
                     " Excluding 5% GST"}
                 </li>
-                <li>
-                  &#x2022;{" "}
-                  {"Minimum distance / day "+
-                    "300 Km"}
-                </li>
+                <li>&#x2022; {"Minimum distance / day " + "300 Km"}</li>
                 <li>
                   &#x2022;{" "}
                   {"extra amount per km ₹" + carList[selectedCar.index].extraKm}
                 </li>
                 <li>
-                  &#x2022; extra charges driver allowance Day ₹400 / Night(11:00 PM to 5:00 AM) ₹300 
+                  &#x2022; Driver allowance Day ₹400 / Night(11:00 PM to 5:00
+                  AM) ₹300
                 </li>
                 <li>
-                  &#x2022;Opening KM/Time and Closing Time/Km will calculate from Our garage to garage
+                  &#x2022;Opening KM/Time and Closing Time/Km will calculate
+                  from Our garage to garage
                 </li>
                 <li>
                   &#x2022; All Parking ,Toll ,Border Tax wherever applicable
                   will be charge on extra
-                </li>
-                <li>
-                  &#x2022; One Day means one calendar day (from midnight 11 to
-                  midnight 11)
                 </li>
               </ul>
             )}
@@ -198,7 +193,8 @@ const RideSelector = ({  pickupCoordinates,
                   will be charge on extra
                 </li>
                 <li>
-                  &#x2022;Opening KM/Time and Closing Time/Km will calculate from Our garage to garage
+                  &#x2022;Opening KM/Time and Closing Time/Km will calculate
+                  from Our garage to garage
                 </li>
               </ul>
             )}
@@ -220,59 +216,64 @@ const RideSelector = ({  pickupCoordinates,
                   &#x2022; {"Extra charge per km : ₹" + selectedCar.extraKm}
                 </li>
                 <li>
-                  &#x2022;Opening KM/Time and Closing Time/Km will calculate from Our garage to garage
+                  &#x2022;Opening KM/Time and Closing Time/Km will calculate
+                  from Our garage to garage
                 </li>
               </ul>
             )}
             {formType === "Airport" && (
               <ul className="flex flex-col gap-2">
                 <li>
-                  &#x2022;{ "Airport Transfer (AIRPORTDROP) :"+price+ "( Excluding 5% GST )"},
+                  &#x2022;
+                  {"Airport Transfer (AIRPORTDROP) :" +
+                    price +
+                    "( Excluding 5% GST )"}
+                  ,
                 </li>
                 <li>&#x2022; Toll and parking extra.</li>
                 <li>
-                    &#x2022; {"Extra charge per hour : ₹" + selectedCar.extraHr}
-                  </li>
-                  <li>
-                    &#x2022; {"Extra charge per km : ₹" + selectedCar.extraKm}
-                  </li>
+                  &#x2022; {"Extra charge per hour : ₹" + selectedCar.extraHr}
+                </li>
+                <li>
+                  &#x2022; {"Extra charge per km : ₹" + selectedCar.extraKm}
+                </li>
               </ul>
             )}
           </PopupContent>
         </Popup>
       )}
     </Wrapper>
-  )
-}
+  );
+};
 
-export default RideSelector
+export default RideSelector;
 
-const Wrapper=tw.div`
-flex-1 overflow-y-scroll flex flex-col `
+const Wrapper = tw.div`
+flex-1 overflow-y-scroll flex flex-col `;
 
-const Title=tw.div`
+const Title = tw.div`
 text-gray-500 text-center text-lg font-medium shadow-md py-2 border-b 
-`
-const Carlist=tw.div`
+`;
+const Carlist = tw.div`
 overflow-y-scroll h-[100vh] p-2
-`
-const Car=tw.button`
+`;
+const Car = tw.button`
 flex p-4 items-center flex-col md:flex-row gap-4 justify-between border border-gray-400 rounded-xl mb-2 w-full shadow-md
-`
-const Carimg=tw.img`
+`;
+const Carimg = tw.img`
 w-full md:w-24 aspect-square border rounded-full border
-`
-const Price=tw.div`
-text-2xl md:text-xl text-center font-medium `
+`;
+const Price = tw.div`
+text-2xl md:text-xl text-center font-medium `;
 
-const Time=tw.div`
-font-xs text-blue-500 cursor-pointer`
+const Time = tw.div`
+font-xs text-blue-500 cursor-pointer`;
 
-const Service=tw.div`
-font-bold text-2xl `
+const Service = tw.div`
+font-bold text-2xl `;
 
-const CarDetails=tw.div`
-flex flex-col justify-center items-center flex-1`
+const CarDetails = tw.div`
+flex flex-col justify-center items-center flex-1`;
 const Popup = tw.div`
   fixed top-0 left-0 right-0 bottom-0 flex items-center flex-col justify-center bg-black bg-opacity-50 p-2 z-10 text-black
 `;
